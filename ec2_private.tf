c88f9eb4e7ea9385fa141e23db2a1470c0172ede
mkumarlavu <mkumarlavu@gmail.com>
2021-10-19 15:41:12 -0400

    Added ec2 bastion and private Ip

--- ec2_private.tf
+++ ec2_private.tf
@@ -3,18 +3,15 @@ module "ec2_private" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "3.2.0"
 
-  for_each = toset(["ec2-app1", "ec2-app2"])
+  name = "${var.environment}-private_host"
 
-  name = "instance-${each.key}-${var.environment}-private_host"
-
-  ami                    = data.aws_ami.ubuntu.id
+  ami                    = data.aws_ami.amzlinux2.id
   instance_type          = var.instance_type
   key_name               = var.instance_keypair
   vpc_security_group_ids = [module.private_main_sg.this_security_group_id]
   subnet_id              = [
-      module.vpc.private_subnets[0],
-      module.vpc.private_subnets[1]
+      module.vpc.private_subnets[0]
   ]
-  user_data = file("{path.module}/app1-install.sh")
+  user_data = file("${path.module}/app1-install.sh")
   tags = local.common_tags
 }
