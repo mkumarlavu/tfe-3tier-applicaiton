14e88dd65629aeff92247ad55b92c061c5de3f37
mkumarlavu <mkumarlavu@gmail.com>
2021-10-19 15:54:08 -0400

    Added ec2 bastion and private Ip

--- ec2_private.tf
+++ ec2_private.tf
@@ -5,13 +5,13 @@ module "ec2_private" {
 
   name = "${var.environment}-private_host"
 
+  count = var.private_instance_count
+
   ami                    = data.aws_ami.amzlinux2.id
   instance_type          = var.instance_type
   key_name               = var.instance_keypair
   vpc_security_group_ids = [module.private_main_sg.this_security_group_id]
-  subnet_id              = [
-      module.vpc.private_subnets[0]
-  ]
+  subnet_ids              = module.vpc.private_subnets
   user_data = file("${path.module}/app1-install.sh")
   tags = local.common_tags
 }
