a738ccebee42dbed5e800f7261f737ef13bcaa50
mkumarlavu <mkumarlavu@gmail.com>
2021-10-19 16:43:43 -0400

    Added ec2 bastion and private Ip

--- ec2_private.tf
+++ ec2_private.tf
@@ -1,17 +1,22 @@
+
 module "ec2_private" {
-  depends_on = [ module.vpc ]
+  depends_on = [ module.vpc ] # VERY VERY IMPORTANT else userdata webserver provisioning will fail
   source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "3.2.0"
-
-  name = "${var.environment}-private_host"
-
-  count = var.private_instance_count
-
+  version = "2.17.0"
+  # insert the 10 required variables here
+  name                   = "${var.environment}-private_instance"
   ami                    = data.aws_ami.amzlinux2.id
   instance_type          = var.instance_type
   key_name               = var.instance_keypair
-  vpc_security_group_ids = [module.private_main_sg.this_security_group_id]
-  subnet_id              = module.vpc.private_subnets
+  #monitoring             = true
+  vpc_security_group_ids = [module.private_sg.this_security_group_id]
+  subnet_ids = [
+    module.vpc.private_subnets[0],
+    module.vpc.private_subnets[1]
+  ]  
+  instance_count         = var.private_instance_count
   user_data = file("${path.module}/app1-install.sh")
   tags = local.common_tags
 }
+
+
