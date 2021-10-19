d22a5c3af77582ef635d46868b61256623cb7283
mkumarlavu <mkumarlavu@gmail.com>
2021-10-19 15:35:59 -0400

    Added ec2 bastion and private Ip

--- ec2_bastion.tf
+++ ec2_bastion.tf
@@ -1,10 +1,10 @@
 module "ec2_public" {
-  source  = "terraform-aws-modules/ec2-instance/aws//examples/complete"
+  source  = "terraform-aws-modules/ec2-instance/aws"
   version = "3.2.0"
 
   name = "${var.environment}-bastion_host"
 
-  ami                    = data.aws_ami.ubuntu.id
+  datasource_ami         = data.aws_ami.ubuntu.id
   instance_type          = var.instance_type
   key_name               = var.instance_keypair
   subnet_id              = module.vpc.public_subnets[0]
